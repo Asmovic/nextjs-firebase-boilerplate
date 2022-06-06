3d2d92db9cb9c86f2e9d2465d77b662af74e2e8e
Asmovic <asogba007@gmail.com>
2022-06-06 11:28:15 +0100

    Initial commit:

---
+++ src/pages/users.js
@@ -0,0 +1,40 @@
+import { Box, Flex, Heading, Stack } from "@chakra-ui/core";
+import React, { useEffect, useState } from "react";
+import EachUser from "../components/eachUser";
+import UserForm from "../components/userForm";
+import { fireStore } from "../firebase";
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    return fireStore.collection("users").onSnapshot((snapshot) => {
+      const usersData = [];
+      snapshot.forEach((user) =>
+        usersData.push({ ...user.data(), id: user.id })
+      );
+      setUsers(usersData);
+    });
+  }, []);
+
+  const addUser = (user) => {
+    console.log(user);
+    fireStore.collection("users").add(user);
+  };
+  return (
+    <Box>
+      <Flex flexWrap="wrap" mt={24} justify="space-around">
+        <>
+          <UserForm addUser={addUser} />
+          <Box w="md">
+            <Heading>View your users here</Heading>
+            <Stack pb={12} spacing={10} mt={8}>
+              {users.map((user) => (
+                <EachUser key={user.id} id={user.id} user={user} />
+              ))}
+            </Stack>
+          </Box>
+        </>
+      </Flex>
+    </Box>
+  );
+}
